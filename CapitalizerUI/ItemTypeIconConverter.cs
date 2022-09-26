4d7bf719ce3c54a85305d45b44632f853cc72902
SlimSoftware <slimsoftware512@gmail.com>
2022-09-26 20:03:38 +0200

    Replace text with icon in type datagrid column

---
+++ CapitalizerUI/ItemTypeIconConverter.cs
@@ -0,0 +1,44 @@
+﻿using CapitalizerLib.Models;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace CapitalizerUI
+{
+    internal class ItemTypeIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                CapitalizableType itemType = (CapitalizableType)value;
+
+                if (itemType == CapitalizableType.Folder)
+                {
+                    return "\uF12B";
+                }
+                else
+                {
+                    return "\uE7C3";
+                }
+            } 
+            catch
+            {
+                return "\uE7C3";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            string iconGlyph = (string)value;
+
+            if (iconGlyph == "\uF12B")
+            {
+                return CapitalizableType.Folder;
+            } 
+            else
+            {
+                return CapitalizableType.File;
+            }
+        }
+    }
+}
